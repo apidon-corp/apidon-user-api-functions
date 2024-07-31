3d15ed60c1d22a298bbbb89a7160cfc757e2b9d6
aboveStars <yunuskorkmazis384@outlook.com>
2024-08-01 00:56:14 +0300

    Minor changes have been made.

--- firebase/adminApp.ts
+++ firebase/adminApp.ts
@@ -1,17 +1,14 @@
 import * as admin from "firebase-admin";
 
-import {
-  keys,
-  environment,
-  serviceAccounts,
-  storageBucketIds,
-} from "../config";
+import {environment, serviceAccounts, storageBucketIds} from "../config";
 
 if (!admin.apps.length) {
   const serviceAccount =
     environment === "development" || environment === "localPreview" ?
       (serviceAccounts.developmentAndLocalPreviewAccount as admin.ServiceAccount) :
-      (keys.SERVICE_ACCOUNT_OBJECT as admin.ServiceAccount);
+      environment === "preview" ?
+        (serviceAccounts.testAccount as admin.ServiceAccount) :
+        (serviceAccounts.developmentAndLocalPreviewAccount as admin.ServiceAccount);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -26,6 +23,8 @@ export const appCheck = admin.appCheck();
 const storageBucketId =
   environment === "development" || environment === "localPreview" ?
     storageBucketIds.developmentAndLocalPreviewAccount :
-    keys.STORAGE_BUCKET_ID;
+    environment === "preview" ?
+      storageBucketIds.testAccount :
+      storageBucketIds.developmentAndLocalPreviewAccount;
 
 export const bucket = admin.storage().bucket(storageBucketId);
