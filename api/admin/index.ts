e4963834c2b73cb1c529f9150afd2d2ae1c78291
aboveStars <yunuskorkmazis384@outlook.com>
2024-09-19 23:08:36 +0300

    Minor changes.

--- api/admin/index.ts
+++ api/admin/index.ts
@@ -1,21 +1,5 @@
 import {getAllPosts} from "./getAllPosts";
 import {updatePostStatus} from "./updatePostStatus";
 import {updateVerifiedStatus} from "./updateVerifiedStatus";
-import {convertCollectors} from "./convertCollectors";
-import {convertComments} from "./convertComments";
-import {convertRate} from "./convertRate";
-import {convertPosts} from "./convertPosts";
-import {convertCollectibles} from "./convertCollectibles";
-import {convertNotifications} from "./convertNotifications";
 
-export {
-  getAllPosts,
-  updatePostStatus,
-  updateVerifiedStatus,
-  convertCollectors,
-  convertComments,
-  convertRate,
-  convertPosts,
-  convertCollectibles,
-  convertNotifications,
-};
+export {getAllPosts, updatePostStatus, updateVerifiedStatus};
