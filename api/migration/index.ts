64b9489065ec75120dc9226d6f7176f037518851
aboveStars <yunuskorkmazis384@outlook.com>
2025-01-12 18:43:36 +0300

    Migration API is removed.

--- api/migration/index.ts
+++
@@ -1,2 +0,0 @@
-import {migratePost} from "./migratePost";
-export {migratePost};
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import * as payment from "../api/payment";
 import * as identity from "../api/identity";
 import * as admin from "../api/admin";
 
-import * as migration from "../api/migration";
 
 export {user};
 export {collectible};
@@ -14,4 +13,4 @@ export {post};
 export {payment};
 export {identity};
 export {admin};
-export {migration};
+
