5f30536ed06fa4765499a5dbf84f03cd8c906101
aboveStars <yunuskorkmazis384@outlook.com>
2025-01-27 14:07:22 +0300

    Minor changes.

--- firebase/adminApp.ts
+++ firebase/adminApp.ts
@@ -1,12 +1,12 @@
-import { initializeApp } from "firebase-admin/app";
-import { getAppCheck } from "firebase-admin/app-check";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
-import { getStorage } from "firebase-admin/storage";
+import {initializeApp} from "firebase-admin/app";
+import {getAppCheck} from "firebase-admin/app-check";
+import {getAuth} from "firebase-admin/auth";
+import {getFirestore} from "firebase-admin/firestore";
+import {getStorage} from "firebase-admin/storage";
 
-const app = initializeApp()
+const app = initializeApp();
 
-export const auth = getAuth(app)
-export const firestore = getFirestore(app)
-export const appCheck = getAppCheck(app)
-export const bucket =  getStorage(app).bucket()
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
+export const appCheck = getAppCheck(app);
+export const bucket = getStorage(app).bucket();
