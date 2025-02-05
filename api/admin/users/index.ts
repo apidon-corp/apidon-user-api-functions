e5f715cc1164e076058182c322033b6249e8c7e6
aboveStars <yunuskorkmazis384@outlook.com>
2025-02-05 18:32:19 +0300

    Minor changes.

--- api/admin/users/index.ts
+++ api/admin/users/index.ts
@@ -2,5 +2,5 @@ import {updateVerifiedStatus} from "./updateVerifiedStatus";
 import {banUser} from "./banUser";
 import {unBanUser} from "./unBanUser";
 import {increaseCollectibleLimit} from "./increaseCollectibleLimit";
-import {getReportedUsers} from "./getReportedUsers"
-export {updateVerifiedStatus, banUser, unBanUser, increaseCollectibleLimit,getReportedUsers};
+import {getReportedUsers} from "./getReportedUsers";
+export {updateVerifiedStatus, banUser, unBanUser, increaseCollectibleLimit, getReportedUsers};
